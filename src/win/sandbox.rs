b08a446f9aa4de69df01253f7e73ef1d5eaffb76
rherv <137662855+rherv@users.noreply.github.com>
2024-01-09 14:59:16 -0800

    Add sandbox detection via recent files count

--- src/win/sandbox.rs
+++ src/win/sandbox.rs
@@ -1,6 +1,10 @@
+use windows::core::PCWSTR;
+use windows::Win32::Foundation::{HANDLE, INVALID_HANDLE_VALUE};
+use windows::Win32::Storage::FileSystem::{FindFirstFileW, FindNextFileW, WIN32_FIND_DATAW};
 use windows::Win32::System::SystemInformation::{
     GetSystemInfo, GlobalMemoryStatusEx, MEMORYSTATUSEX, SYSTEM_INFO,
 };
+use windows::Win32::UI::Shell::{FOLDERID_Recent, SHGetKnownFolderPath, KNOWN_FOLDER_FLAG};
 
 pub fn check_cpu_with_threshold(count: u32) -> bool {
     let mut system_info: SYSTEM_INFO = unsafe { std::mem::zeroed() };
@@ -23,4 +27,39 @@ pub fn check_ram_with_threshold(gigabyte: u32) -> bool {
     let min: u64 = (gigabyte as u64) * 1024 * 1024 * 1024;
 
     return memory_status.ullTotalPhys < min;
+}
+
+pub fn check_recent_files(count: u32) -> bool {
+    let mut file_count: u32 = 0;
+
+    let mut recent_files: Vec<u16>;
+
+    unsafe {
+        match SHGetKnownFolderPath(&FOLDERID_Recent, KNOWN_FOLDER_FLAG(0), None) {
+            Ok(folder_path) => {
+                recent_files = folder_path.as_wide().iter().copied().collect();
+            }
+            Err(_) => {
+                return false;
+            }
+        }
+    }
+
+    recent_files.extend(&['\\' as u16, '*' as u16, 0]);
+
+    let mut find_data: WIN32_FIND_DATAW = unsafe { std::mem::zeroed() };
+    let file_handle: HANDLE = unsafe {
+        match FindFirstFileW(PCWSTR(recent_files.as_ptr()), &mut find_data) {
+            Ok(file) => file,
+            Err(_) => return true,
+        }
+    };
+
+    if file_handle != INVALID_HANDLE_VALUE {
+        while unsafe { FindNextFileW(file_handle, &mut find_data).is_ok() } {
+            file_count += 1;
+        }
+    }
+
+    return file_count < count;
 }
--- tests/win_test.rs
+++ tests/win_test.rs
@@ -2,21 +2,64 @@ use anti_analysis::win;
 
 #[cfg(test)]
 mod win_test {
-    use super::win::vm::*;
     use super::win::sandbox::*;
+    use super::win::vm::*;
 
     #[test]
     fn test_vm_check_all() {
         println!("Windows Virtual Machine Check");
-        println!("files\t: {}",   if check_all_files() { "detected" } else { "none" });
-        println!("procs\t: {}",   if check_all_processes() { "detected" } else { "none" });
-        println!("regkeys\t: {}", if check_all_reg_keys() { "detected" } else { "none" });
+        println!(
+            "files\t: {}",
+            if check_all_files() {
+                "detected"
+            } else {
+                "none"
+            }
+        );
+        println!(
+            "procs\t: {}",
+            if check_all_processes() {
+                "detected"
+            } else {
+                "none"
+            }
+        );
+        println!(
+            "regkeys\t: {}",
+            if check_all_reg_keys() {
+                "detected"
+            } else {
+                "none"
+            }
+        );
     }
 
     #[test]
-    fn test_misc_check() {
+    fn test_sandbox_check() {
         println!("Windows Sandbox Detection");
-        println!("cpu\t: {}", if check_cpu_with_threshold(2) { "detected" } else { "none" });
-        println!("ram\t: {}", if check_ram_with_threshold(4) { "detected" } else { "none" });
+        println!(
+            "cpu\t\t: {}",
+            if check_cpu_with_threshold(2) {
+                "detected"
+            } else {
+                "none"
+            }
+        );
+        println!(
+            "ram\t\t: {}",
+            if check_ram_with_threshold(4) {
+                "detected"
+            } else {
+                "none"
+            }
+        );
+        println!(
+            "recent\t: {}",
+            if check_recent_files(5) {
+                "detected"
+            } else {
+                "none"
+            }
+        );
     }
 }
